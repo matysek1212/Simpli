4b5c6f9898407d5b84d01e2931974b85f7994c17
Matyáš Bartoň <matyonytb@gmail.com>
2025-11-26 22:53:30 +0100

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,1 +1,15 @@
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.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);
+
+export default async function handler(req, res) {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{ price: process.env.PRICE_ID, quantity: 1 }],
+    mode: 'subscription',
+    success_url: 'https://matysek1212.github.io/success',
+    cancel_url: 'https://matysek1212.github.io/cancel',
+  });
+
+  res.json({ id: price_1SXq3Q0ni8h4CvJZuwGIoGCH });
+}
 
